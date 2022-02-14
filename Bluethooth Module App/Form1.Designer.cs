54c5586c17f742a426e284f583079e66b8da326d
TobanCZ <65807074+TobanCZ@users.noreply.github.com>
2022-02-14 21:01:10 +0100

    y

---
+++ Bluethooth Module App/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿
+namespace Bluethooth_Module_App
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Status_Label = new System.Windows.Forms.Label();
+            this.Status_txt = new System.Windows.Forms.Label();
+            this.Status_Timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // Status_Label
+            // 
+            this.Status_Label.AutoSize = true;
+            this.Status_Label.Location = new System.Drawing.Point(12, 9);
+            this.Status_Label.Name = "Status_Label";
+            this.Status_Label.Size = new System.Drawing.Size(40, 13);
+            this.Status_Label.TabIndex = 0;
+            this.Status_Label.Text = "Status:";
+            // 
+            // Status_txt
+            // 
+            this.Status_txt.AutoSize = true;
+            this.Status_txt.Location = new System.Drawing.Point(48, 9);
+            this.Status_txt.Name = "Status_txt";
+            this.Status_txt.Size = new System.Drawing.Size(61, 13);
+            this.Status_txt.TabIndex = 1;
+            this.Status_txt.Text = "Status Text";
+            // 
+            // Status_Timer
+            // 
+            this.Status_Timer.Tick += new System.EventHandler(this.Status_Update);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Status_txt);
+            this.Controls.Add(this.Status_Label);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.OnLoad);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Status_Label;
+        private System.Windows.Forms.Label Status_txt;
+        private System.Windows.Forms.Timer Status_Timer;
+    }
+}
+
---
+++ Bluethooth Module App/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bluethooth_Module_App
+{
+    public partial class Form1 : Form
+    {
+        static SerialPort port;
+        bool PortFind = false;
+        static string Message = "";
+        string[] ports;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Status_Update(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OnLoad(object sender, EventArgs e)
+        {
+            FindPort();
+        }
+
+        private void Port_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            string msg = port.ReadExisting();
+            GetAllMessage(msg);
+        }
+
+        void GetAllMessage(string msg)
+        {
+            Message += msg;
+            int index = Message.IndexOf("\r\n");
+            if (index != -1)
+            {
+                string message = Message.Substring(0, index + 2);
+                Message = Message.Remove(0, index + 2);
+                Console.Write(message);
+            }
+
+            if (Message.Contains("\r\n"))
+            {
+                GetAllMessage("");
+            }
+        }
+
+        void FindPort()
+        {
+            ports = SerialPort.GetPortNames();
+
+            port = new SerialPort("COM4");
+            port.DataReceived += Port_DataReceived;
+            port.BaudRate = 9600;
+            port.Open();
+            port.Write("AT\r\n");
+            while (PortFind == false) ;
+        }
+    }
+}
+
